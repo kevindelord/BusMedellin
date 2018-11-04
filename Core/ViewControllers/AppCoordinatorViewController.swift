dce4085f2544a9ebaf2478a55c6ae6858440ab6d
kevindelord <delord.kevin@gmail.com>
2018-11-04 16:32:01 +0100

    Show and hide search results

--- Core/ViewControllers/AppCoordinatorViewController.swift
+++ Core/ViewControllers/AppCoordinatorViewController.swift
@@ -1,5 +1,5 @@
 //
-//  BMMapContainerViewController.swift
+//  AppCoordinatorViewController.swift
 //  BusMedellin
 //
 //  Created by Kevin Delord on 15/10/16.
@@ -60,8 +60,21 @@ protocol Coordinator {
 	func prepare(for segue: UIStoryboardSegue, on container: ViewContainer)
 }
 
-// TODO: RENAME into AppCoordinator
-class MapCoordinator {
+protocol SearchResultCoordinator {
+
+	/// View Height Constraint
+	var searchResultConstraint: NSLayoutConstraint? { get set }
+
+	/// Show the container view displaying the search results.
+	func showSearchResults()
+
+	/// Hide the container view displaying the search results.
+	func hideSearchResults()
+}
+
+class AppCoordinator {
+
+	var searchResultConstraint	: NSLayoutConstraint?
 
 	private let routeManager	= RouteManager()
 	private var container 		: ViewContainer? // TODO: what is this ?
@@ -69,7 +82,18 @@ class MapCoordinator {
 	private var routesContainer	: ContentView?
 }
 
-extension MapCoordinator: Coordinator {
+extension AppCoordinator: SearchResultCoordinator {
+
+	func showSearchResults() {
+		self.searchResultConstraint?.constant = 150
+	}
+
+	func hideSearchResults() {
+		self.searchResultConstraint?.constant = 0
+	}
+}
+
+extension AppCoordinator: Coordinator {
 
 	// TODO: refactor and improve this function.
 	func prepare(for segue: UIStoryboardSegue, on container: ViewContainer) {
@@ -123,7 +147,7 @@ extension MapCoordinator: Coordinator {
 
 // MARK: - ContentViewDelegate
 
-extension MapCoordinator: ContentViewDelegate {
+extension AppCoordinator: ContentViewDelegate {
 
 	/// Update/Reload all content views (Map and Routes Containers)
 	func reloadContentViews() {
@@ -132,10 +156,17 @@ extension MapCoordinator: ContentViewDelegate {
 
 		self.mapView?.update(availableRoutes: availableRoutes, selectedRoute: selectedRoute)
 		self.routesContainer?.update(availableRoutes: availableRoutes, selectedRoute: selectedRoute)
+
+		if (availableRoutes.isEmpty == false) {
+			self.showSearchResults()
+		} else {
+			self.hideSearchResults()
+		}
 	}
 
 	/// Only update/reload the map content view.
 	/// The routesContainer should not be updated as it is the input view for the user to select a new route.
+	/// The routesContainer is already shown.
 	func reloadContentViewsForSelectedRoute() {
 		let availableRoutes = self.routeManager.availableRoutes
 		let selectedRoute = self.routeManager.selectedRoute
@@ -146,7 +177,7 @@ extension MapCoordinator: ContentViewDelegate {
 
 // MARK: - RouteManagerDelegate
 
-extension MapCoordinator: RouteManagerDelegate {
+extension AppCoordinator: RouteManagerDelegate {
 
 	func select(route: Route) {
 		self.routeManager.select(route: route)
@@ -156,17 +187,19 @@ extension MapCoordinator: RouteManagerDelegate {
 	func cancelSearch() {
 		self.routeManager.cancelSearch()
 		self.reloadContentViews()
+		self.hideSearchResults()
 	}
 }
 
-// TODO: Rename into AppCoordinatorViewController
-class BMMapContainerViewController	: UIViewController, ViewContainer {
+class AppCoordinatorViewController	: UIViewController, ViewContainer {
 
 	var coordinator					: Coordinator
 
+	@IBOutlet private weak var searchResultHeight : NSLayoutConstraint?
+
 	required init?(coder aDecoder: NSCoder) {
 		// Init coordinator depending on local needs.
-		self.coordinator = MapCoordinator()
+		self.coordinator = AppCoordinator()
 
 		super.init(coder: aDecoder)
 	}
@@ -174,6 +207,8 @@ class BMMapContainerViewController	: UIViewController, ViewContainer {
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 		super.prepare(for: segue, sender: sender)
 
+		(self.coordinator as? AppCoordinator)?.searchResultConstraint = self.searchResultHeight
 		self.coordinator.prepare(for: segue, on: self)
+
 	}
 }

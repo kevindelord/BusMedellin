43b4c86a1e4af8154f7f2319b902568504ce4804
kevindelord <delord.kevin@gmail.com>
2018-11-18 19:55:19 +0100

    Reduce HUD height

--- App/Map/HUD/HUD.swift
+++ App/Map/HUD/HUD.swift
@@ -10,7 +10,7 @@ import UIKit
 
 struct HUD {
 
-	static let standardHeight			= CGFloat(5.0)
+	static let standardHeight			= CGFloat(4.0)
 	static let indicatorColor			= Color.blue
 	static let reducedSizeRatio			= CGFloat(0.6)
 	static let slidedOriginRatio		= CGFloat(0.3)

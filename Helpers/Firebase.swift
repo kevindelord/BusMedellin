4c0401fd17a3daa72dcc13a9e97db9802b97327d
kevindelord <delord.kevin@gmail.com>
2018-11-18 16:50:29 +0100

    Firebase: decrease log level

--- Helpers/Firebase.swift
+++ Helpers/Firebase.swift
@@ -15,6 +15,8 @@ import Firebase
 struct Firebase {
 
 	static func setup() {
+		// Set the Firebase log to the minimum required.
+		FirebaseConfiguration.shared.setLoggerLevel(.min)
 		// Use Firebase library to configure APIs
 		FirebaseApp.configure()
 	}
